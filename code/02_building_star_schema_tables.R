6c0ece26cc5936141cbc1380b5ab991986f20ecb
Jose Teran <teran.joseandres@gmail.com>
2024-09-18 00:11:41 -0500

    Added create fact table and orgcode dim table

--- code/02_building_star_schema_tables.R
+++ code/02_building_star_schema_tables.R
@@ -1,2 +1,18 @@
 # Build star schema tables
 
+# #--------------------Create the Fact Table-----------------
+fact_support_ticket <- clean_df
+
+# #--------------------Create the Dimension Tables-----------------
+
+# * Orgcode Dimension table-----------------
+dim_orgcode <- clean_df %>%
+  select(orgcode) %>%
+  distinct() %>%
+  arrange(orgcode) %>%
+  mutate(orgcode_id = row_number())
+
+# Update fact table with the org code data
+fact_support_ticket <- fact_support_ticket %>%
+  left_join(dim_orgcode, by = "orgcode") %>%
+  select(-orgcode)
